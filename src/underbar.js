4b93da74b181787c54fe06ca1fb4f8925d30ecda
Jackson Pham <jackson.pham@gmail.com>
2020-11-17 23:01:20 -0800

    "Completed Every Function"

--- src/underbar.js
+++ src/underbar.js
@@ -295,6 +295,25 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    /*
+    Call reduce and pass true as the accumulator, collection, iterator
+      Callback function
+        If accumulator === false
+          return false
+        else
+          return iterator of current item
+    */
+    if (iterator === undefined) {
+      iterator = _.identity;
+    }
+
+    return _.reduce(collection, function(accumulator, item) {
+      if (accumulator === false || !iterator(item)) {
+        return false;
+      } else if (iterator(item)) {
+        return true;
+      }
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
